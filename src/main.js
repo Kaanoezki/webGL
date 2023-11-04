0beca0e7f2e71f5718e3f1dafad580789f4ed510
Kaanoezki <135235440+Kaanoezki@users.noreply.github.com>
2023-11-04 15:45:59 +0100

    erster commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+//get canvas element
+const canvas = document.getElementById(webgl-canvas);
+
+//webGL innit
+const gl = canvas.getContext("webgl");
+
+//errortest
+if(!gl){
+    consolelog("error, webGL not supported")  ;
+}
+else{
+    
+}
